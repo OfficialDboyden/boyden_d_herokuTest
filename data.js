3cab5a6184a49dbd66c8296aef3e61f01487023d
Boyden <d_boyden@tss-hr420-dm05.fc.ca>
2019-03-14 17:44:01 -0400

    user data done with pics

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = {
+  name:"Daniel",
+  role:"learnerator",
+
+
+
+  user1: {
+    name: "user1",
+    role: "designer",
+    bio: "some generic text for user1",
+    bioPic: "gnome_child.jpg"
+  },
+
+  user2: {
+    name: "user2",
+    role: "developer",
+    bio: "some generic text for user2",
+    bioPic: "karamja_monkey.png"
+  },
+}
+
+module.exports = data;
